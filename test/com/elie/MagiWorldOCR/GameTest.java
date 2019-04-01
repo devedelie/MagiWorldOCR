39eb3437e2f90b214c3a29b9f1620647b1eb63b8
e210564e <devedelie@gmail.com>
2019-04-01 10:50:03 +0200

    Creation of a test method to check my IntValueRequest Method. The test should get a number, and check if it is between the authorized range in addition to its limit value (strength + agility + intelligence <= level)

--- test/com/elie/MagiWorldOCR/GameTest.java
+++ test/com/elie/MagiWorldOCR/GameTest.java
@@ -1,7 +1,37 @@
 package com.elie.MagiWorldOCR;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    Game game = new Game();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+    @Test
+    public void Given_IntValue_When_RequestValue_Then_CheckValueLimit() {
+        System.setIn(new ByteArrayInputStream(String.format("56%n").getBytes()));
+        Game game = new Game();
+        int value = game.RequestValueMethodForPerformances("Checking the value method", 90);
+        assertEquals(56, value);
+    }
+
+    @Test
+    public void Given_IntValue_When_RequestValue_Then_CheckBadValue_and_Then_CheckGoodValue() {
+        System.setIn(new ByteArrayInputStream(String.format("86%n65%n").getBytes()));
+        Game game = new Game();
+        int value = game.RequestValueMethodForPerformances("Checking the value method", 70);
+        assertEquals(65, value);
+    }
+
 
 }
