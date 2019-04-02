b52cbe15897383a231aa623444e7eb3290092589
e210564e <devedelie@gmail.com>
2019-04-02 10:52:11 +0200

    - Creation of the MagusTest file - Creation of Magus attack functionality. - Fixing the "initialVitality" variable to reclaim the initial value of user's vitality.

--- src/com/elie/MagiWorldOCR/Magus.java
+++ src/com/elie/MagiWorldOCR/Magus.java
@@ -3,15 +3,13 @@ package com.elie.MagiWorldOCR;
 public class Magus extends Character {
     protected int basicAttackFireball;
     protected int specialAttackCare;
+    int initialVitality;
 
     public Magus(String name, int level, int strength, int agility, int intelligence) {
         super(name, level, strength, agility, intelligence);
-
-
+        initialVitality = vitality; // Keeps the original level value to compare when using Special Attack
     }
-
-    int levelFlag = level; // Keeps the original level value to compare when using Special Attack
-
+    
     @Override
     public void BasicAttack(Character opponent, Character currentPlayer) {
         System.out.println("Fire-Ball!");
@@ -23,8 +21,9 @@ public class Magus extends Character {
     public void SpecialAttack(Character opponent, Character currentPlayer) {
         System.out.println("Care!");
         currentPlayer.vitality = currentPlayer.vitality + (currentPlayer.intelligence*2);
-        if (currentPlayer.vitality > levelFlag)
-            currentPlayer.vitality = levelFlag;
+        if (currentPlayer.vitality > initialVitality){
+            currentPlayer.vitality = initialVitality;
+        }
         System.out.println("You have cured yourself. Your new vitality is: " + currentPlayer.vitality);
     }
 
--- test/com/elie/MagiWorldOCR/MagusTest.java
+++ test/com/elie/MagiWorldOCR/MagusTest.java
@@ -1,7 +1,25 @@
 package com.elie.MagiWorldOCR;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MagusTest {
+
+    @Test
+    public void Given_MagusBasicAttack_When_RequestTypeOfAttack_Then_CheckResult() {
+        Magus player = new Magus("Player 1", 100, 33,33,33);
+        Magus opponent = new Magus("Player 2", 100, 22,22,22);
+        player.BasicAttack(opponent,player);
+        assertEquals(467, opponent.getVitality()); // opponent damage
+    }
+
+    @Test
+    public void Given_MagusSpecialAttack_When_RequestTypeOfAttack_Then_CheckResults() {
+        Magus player = new Magus("Player 1", 25, 3,3,3);
+        Magus opponent = new Magus("Player 2", 100, 22,22,22);
+        player.SpecialAttack(opponent,player);
+        assertEquals(125, player.getVitality());  // currentPlayer care
+    }
 
 }
